2ef7578911ac67bff59cb53500882e33bebd9217
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-11-21 21:58:25 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         int sum = 0;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     sum = nums[i] + nums[j] + nums[k];
                     int max = 0;
