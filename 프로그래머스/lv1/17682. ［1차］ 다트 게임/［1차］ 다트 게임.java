085712868ca4534c6a2bcee7659622a5e7e59c22
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-11-24 19:32:52 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.12 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+       
+        stack.push(0);
+        for (int i = 0; i< dartResult.length(); i++) {
+            if (dartResult.charAt(i) == '#') {
+                int num = stack.pop();
+
+                stack.push(num*-1);
+            }
+            else if (dartResult.charAt(i) == '*') {
+                int num = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num2*2);
+                stack.push(num*2);
+            }
+            else if (dartResult.charAt(i) == 'S') {
+                int num = stack.pop();
+                stack.push(num);
+            }
+            else if (dartResult.charAt(i) == 'D') {
+                int num = stack.pop();
+                stack.push(num*num);
+            }
+            else if (dartResult.charAt(i) == 'T') {
+                int num = stack.pop();
+                stack.push(num*num*num);
+            }else {
+                int num = Integer.parseInt(String.valueOf(dartResult.charAt(i)));
+                char num2 = dartResult.charAt(i+1);
+                if(num==1 && num2=='0') {
+                    stack.push(10);
+                    i++;
+                }else {
+                    stack.push(Integer.parseInt(String.valueOf(dartResult.charAt(i))));
+                }
+            }
+        }
+        answer = stack.pop()+stack.pop()+stack.pop();
+        return answer;
+    }
+}
