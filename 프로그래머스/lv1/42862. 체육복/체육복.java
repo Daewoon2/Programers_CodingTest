2d20ec18e378b144e2b511915ddf26d87144ca7f
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-11-28 19:17:32 +0900

    [level 1] Title: 체육복, Time: 0.34 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,29 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int count = 0;
+        int answer = 0;
+
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        for(int i=0; i < lost.length; i++) {
-            for(int j=0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]) {
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
                     lost[i] = -1;
                     reserve[j] = -1;
-                    count++;
+                    answer++;
                     break;
                 }
             }
         }
-        for (int i=0; i < lost.length; i++) {
-            for (int j=0; j < reserve.length; j++) {
-                if(lost[i] == reserve[j]+1 || lost[i] == reserve[j]-1) {
-                    count++;
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j] + 1 || lost[i] == reserve[j] - 1) {
+                    answer++;
                     reserve[j] = -1;
                     break;
-
                 }
             }
         }
-        return n - lost.length + count;
+
+        answer += n - lost.length;
+        return answer;
     }
 }
