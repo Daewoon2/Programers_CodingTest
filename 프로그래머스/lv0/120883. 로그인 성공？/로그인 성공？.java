de2c9a68579931c52662b7dc550ad1aff8ef5c40
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-11-30 15:46:24 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -10,5 +10,24 @@ class Solution {
             } 
         }
         return "fail";
+        
+        //        String answer = "";
+//        //["meosseugi", "1234"]
+//        HashMap<String, String> db_Map = new HashMap<>();
+//
+//        for (String[] str : db) {
+//            db_Map.put(str[0], str[1]);
+//            //["rardss", "123"],
+//            // ["yyoom", "1234"],
+//            // ["meosseugi", "1234"]
+//
+//            if (db_Map.containsKey(id_pw[0])) { //아이디가 있을 때
+//                if (db_Map.get(str[1]).equals(id_pw[1])) { // 아이디 비밀번호 둘다 같을 때
+//                    answer = "login";
+//                } else answer = "wrong pw";
+//            } else answer = "fail";
+//        }
+//
+//        return answer;
     }
 }
