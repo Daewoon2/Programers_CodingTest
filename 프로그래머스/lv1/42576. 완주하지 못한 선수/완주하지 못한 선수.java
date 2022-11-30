ad79083761a9370a305c1c5b6a99551346c76a85
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-11-30 19:44:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 334.91 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,13 +4,15 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
-
+        
         int i;
         for (i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i]))
+            if (!participant[i].equals(completion[i])) {
                 break;
+            }
+            
 
         }
-      return participant[i];
+        return participant[i];
     }
 }
