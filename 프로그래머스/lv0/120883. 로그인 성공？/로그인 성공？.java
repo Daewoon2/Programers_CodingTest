40a078b1e6ef893f7c8744b4e9ba4c119a102e3b
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-12-01 19:14:47 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -2,32 +2,32 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        for (int i = 0; i < db.length; i++) {
-            if (db[i][0].equals(id_pw[0])) {
-                if (db[i][1].equals(id_pw[1])) {
-                    return "login";
-                } else return "wrong pw";
-            } 
-        }
-        return "fail";
+        // for (int i = 0; i < db.length; i++) {
+        //     if (db[i][0].equals(id_pw[0])) {
+        //         if (db[i][1].equals(id_pw[1])) {
+        //             return "login";
+        //         } else return "wrong pw";
+        //     } 
+        // }
+        // return "fail";
         
-        //        String answer = "";
-//        //["meosseugi", "1234"]
-//        HashMap<String, String> db_Map = new HashMap<>();
-//
-//        for (String[] str : db) {
-//            db_Map.put(str[0], str[1]);
-//            //["rardss", "123"],
-//            // ["yyoom", "1234"],
-//            // ["meosseugi", "1234"]
-//
-//            if (db_Map.containsKey(id_pw[0])) { //아이디가 있을 때
-//                if (db_Map.get(str[1]).equals(id_pw[1])) { // 아이디 비밀번호 둘다 같을 때
-//                    answer = "login";
-//                } else answer = "wrong pw";
-//            } else answer = "fail";
-//        }
-//
-//        return answer;
+               String answer = "";
+       //["meosseugi", "1234"]
+       HashMap<String, String> db_Map = new HashMap<>();
+
+       for (String[] str : db) {
+           db_Map.put(str[0], str[1]);
+           //["rardss", "123"],
+           // ["yyoom", "1234"],
+           // ["meosseugi", "1234"]
+
+           if (db_Map.containsKey(id_pw[0])) { //아이디가 있을 때
+               if (db_Map.get(id_pw[0]).equals(id_pw[1])) { // 아이디 비밀번호 둘다 같을 때
+                   answer = "login";
+               } else answer = "wrong pw";
+           } else answer = "fail";
+       }
+
+       return answer;
     }
 }
