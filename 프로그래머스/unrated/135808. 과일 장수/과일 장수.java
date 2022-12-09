d59da9d720a63d3370f9325329978c0a336c82cc
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-12-09 20:30:16 +0900

    [unrated] Title: 과일 장수, Time: 0.33 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
+//         int answer = 0;
+
+//         int count = score.length; //총 사과 개수
+//         int namoji = count%m; //상자를 만들고 남는 사과 개수
+
+//         Arrays.sort(score);
+
+//         for (int i=namoji; i<count; i=i+m) {
+//             int plus = score[i]*m;
+//             answer = answer+plus;
+//         }
+
+//         return answer;
+        
         int answer = 0;
 
         // 배열 역순으로 tScore 배열에 담기
