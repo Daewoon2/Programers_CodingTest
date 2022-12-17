2ed501154f9f3e25a94b1ec3c43cfed81e83f6a3
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-12-17 13:22:40 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 9.52 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,39 +1,32 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        int[] result = new int[4];
-        for (int i = 0; i < survey.length; i++) {
-            char alphabet = survey[i].charAt(0);
-            int choice = choices[i] - 4;
-            switch (alphabet) {
-                case 'R': result[0] += choice; break;
-                case 'T': result[0] -= choice; break;
-                case 'C': result[1] += choice; break;
-                case 'F': result[1] -= choice; break;
-                case 'J': result[2] += choice; break;
-                case 'M': result[2] -= choice; break;
-                case 'A': result[3] += choice; break;
-                case 'N': result[3] -= choice; break;
+           String answer = "";
+        char [][] type = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        int [] score = {0, 3, 2, 1, 0, 1, 2, 3};
+        HashMap<Character, Integer> point = new HashMap<Character, Integer>();
+
+        // 점수 기록할 배열 초기화 
+        for (char[] t : type) {
+            point.put(t[0], 0);
+            point.put(t[1], 0);
+        }
+
+        // 점수 기록 
+        for (int idx = 0; idx < choices.length; idx++){
+            if(choices[idx] > 4){
+                point.put(survey[idx].charAt(1), point.get(survey[idx].charAt(1)) + score[choices[idx]]);
+            } else {
+                point.put(survey[idx].charAt(0), point.get(survey[idx].charAt(0)) + score[choices[idx]]);
             }
         }
-        
-        for (int i = 0; i < 4; i++) {
-            int num = result[i];
-            switch (i) {
-                case 0: 
-                    if (num > 0) {answer += "T";} 
-                    else {answer += "R";} break;
-                case 1:
-                    if (num > 0) {answer += "F";} 
-                    else {answer += "C";} break;
-                case 2: 
-                    if (num > 0) {answer += "M";} 
-                    else {answer += "J";} break;
-                case 3: 
-                    if (num > 0) {answer += "N";} 
-                    else {answer += "A";} break;
-            }
+
+        // 지표 별 점수 비교 후 유형 기입
+        for (char[] t : type) {
+            answer += (point.get(t[1]) <= point.get(t[0])) ? t[0] : t[1];
         }
+
         return answer;
     }
 }
