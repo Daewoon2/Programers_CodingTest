fe728db08a2f9068ca8e4a23874a55ecf08a4b56
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-12-20 18:59:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -2,21 +2,27 @@ class Solution {
     public String[] solution(String[] quiz) {
         String[] answer = new String[quiz.length];
 
+        // [ 0 1 2 3  4 ,  0 1 2 3 4
+        // ["3 - 4 = -3", "5 + 6 = 11"]
         for (int i = 0; i < quiz.length; i++) {
             String[] quizOX = quiz[i].split(" ");
+            int front = Integer.parseInt(quizOX[0]);
+            int back = Integer.parseInt(quizOX[2]);
+            int result = Integer.parseInt(quizOX[4]);
+            
             if (quizOX[1].equals("+")) {
-                if (Integer.parseInt(quizOX[0]) + Integer.parseInt(quizOX[2]) == Integer.parseInt(quizOX[4])) {
+                if (front + back == result) {
                     answer[i] = "O";
                 } else {
                     answer[i] = "X";
                 }
             } else if (quizOX[1].equals("-")) {
-                if (Integer.parseInt(quizOX[0]) - Integer.parseInt(quizOX[2]) == Integer.parseInt(quizOX[4])) {
+                if (front - back == result) {
                     answer[i] = "O";
                 } else {
                     answer[i] = "X";
-                 }
-             }
+                }
+            }
         }
         return answer;
     }
