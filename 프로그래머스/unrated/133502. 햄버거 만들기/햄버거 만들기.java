83e027206589c5a8a040eae49db35bd8075060cf
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2022-12-22 21:15:04 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.21 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,7 +8,8 @@ class Solution {
         for (int i = 0; i < ingredient.length; i++) {
             stack.push(ingredient[i]);
             if (stack.size() > 3) {
-                if (stack.get(stack.size() - 4) == 1 && stack.get(stack.size() - 3) == 2 &&
+                if (stack.get(stack.size() - 4) == 1 && 
+                    stack.get(stack.size() - 3) == 2 &&
                         stack.get(stack.size() - 2) == 3 && stack.get(stack.size() - 1) == 1) {
                     stack.pop();
                     stack.pop();
