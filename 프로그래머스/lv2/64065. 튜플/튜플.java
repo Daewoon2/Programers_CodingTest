430f6110a2341a5bbf5e32dbbef44716f8398abd
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2023-01-07 12:14:09 +0900

    [level 2] Title: 튜플, Time: 0.59 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -4,16 +4,28 @@ class Solution {
     public int[] solution(String s) {
         Set<String> set = new LinkedHashSet<>();
         String[] str = s.substring(1, s.length() - 1).split("},");
+        // {2}, {2,1}, {2,1,3}, {2,1,3,4,}
+        
         for (int i = 0; i < str.length; i++) {
             str[i] = str[i].replace("{", "");
             str[i] = str[i].replace("}", "");
         }
+        // 2, 2,1, 2,1,3, 2,1,3,4
 
-        Arrays.sort(str, (a, b) -> (a.length() - b.length()));
+        Arrays.sort(str,new Comparator<String>(){
+            public int compare(String o1, String o2){
+
+                return Integer.compare(o1.length(), o2.length());
+            }  // 2,1,3,4 2,1,3, 2,1, 2
+        });
+       
+
         for (String a : str) {
             String[] arr = a.split(",");
+            // 2, 1, 3, 4 2, 1, 3, 2, 1, 2
+            
             for (String b : arr) {
-                set.add(b);
+                set.add(b); // 2, 1, 3, 4
             }
         }
 
