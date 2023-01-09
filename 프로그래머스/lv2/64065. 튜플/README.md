919021580c7b3ea5cd50d1c3d8a4567f83fe2642
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2023-01-09 21:29:32 +0900

    [level 2] Title: 튜플, Time: 0.93 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/README.md
+++ 프로그래머스/lv2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 71.1 MB, 시간: 0.59 ms
+메모리: 72.7 MB, 시간: 0.93 ms
 
 ### 구분
 
--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -12,17 +12,17 @@ class Solution {
         }
         // 2, 2,1, 2,1,3, 2,1,3,4
 
-        Arrays.sort(str,new Comparator<String>(){
+        Arrays.sort(str, new Comparator<String>(){
             public int compare(String o1, String o2){
 
                 return Integer.compare(o1.length(), o2.length());
-            }  // 2,1,3,4 2,1,3, 2,1, 2
+            }  // 2,1,3,4   2,1,3,   2,1,   2
         });
        
 
         for (String a : str) {
             String[] arr = a.split(",");
-            // 2, 1, 3, 4 2, 1, 3, 2, 1, 2
+            // 2, 1, 3, 4, 2, 1, 3, 2, 1, 2
             
             for (String b : arr) {
                 set.add(b); // 2, 1, 3, 4
