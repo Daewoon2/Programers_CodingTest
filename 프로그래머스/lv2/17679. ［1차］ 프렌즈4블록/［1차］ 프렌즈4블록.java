6cc79dd3fa8f3ae70a3949509f66a4c4dee52126
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2023-04-03 19:55:30 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.29 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -5,7 +5,7 @@ class Solution {
         char[][] map = new char[m][n];
 
         // 2차워배열로 보드 글자들을 담는다.
-        for(int i = 0 ; i < m ; ++i) {
+        for(int i = 0 ; i < m ; i++) {
             map[i] = board[i].toCharArray();
         }
 
@@ -23,10 +23,10 @@ class Solution {
 
     // 떨어지는 블록 체크
     private void dropBlock(int m, int n, char[][] map) {
-        for(int c = 0 ; c < n ; ++c) {
-            for(int r = m - 1 ; r >= 0 ; --r) {
+        for(int c = 0 ; c < n ; c++) {
+            for(int r = m - 1 ; r >= 0 ; r--) {
                 if(map[r][c] == '.') {
-                    for(int nr = r - 1 ; nr >= 0 ; --nr) {
+                    for(int nr = r - 1 ; nr >= 0 ; nr--) {
                         if(map[nr][c] != '.') {
                             map[r][c] = map[nr][c];
                             map[nr][c] = '.';
@@ -43,15 +43,15 @@ class Solution {
         int cnt = 0;
         boolean[][] isChecked = new boolean[m][n];
 
-        for(int i = 0 ; i < m - 1; ++i) {
-            for(int j = 0 ; j < n - 1; ++j) {
+        for(int i = 0 ; i < m - 1; i++) {
+            for(int j = 0 ; j < n - 1; j++) {
                 if(map[i][j] == '.') continue;
                 checkFour(map, isChecked, i, j);
             }
         }
 
-        for(int i = 0 ; i < m ; ++i) {
-            for(int j = 0 ; j < n ; ++j) {
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
                 if(isChecked[i][j]) {
                     cnt++;
                     map[i][j] = '.';
@@ -66,14 +66,14 @@ class Solution {
     private void checkFour(char[][] map, boolean[][] isChecked, int i, int j) {
         char block = map[i][j];
 
-        for(int r = i ; r < i + 2 ; ++r) {
-            for(int c = j ; c < j + 2 ; ++c) {
+        for(int r = i ; r < i + 2 ; r++) {
+            for(int c = j ; c < j + 2 ; c++) {
                 if(map[r][c] != block) return;
             }
         }
 
-        for(int r = i ; r < i + 2 ; ++r) {
-            for(int c = j ; c < j + 2 ; ++c) {
+        for(int r = i ; r < i + 2 ; r++) {
+            for(int c = j ; c < j + 2 ; c++) {
                 isChecked[r][c] = true;
             }
         }
