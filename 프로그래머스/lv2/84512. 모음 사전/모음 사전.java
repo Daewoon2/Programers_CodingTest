631f8ee591bdf73d34b7f7ed72bc404c303ba6cd
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2023-04-17 18:25:31 +0900

    [level 2] Title: 모음 사전, Time: 4.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+   static String[] arr;
+    static List<String> list;
+
+    public int solution(String word) {
+        int answer = 0;
+
+        arr = new String[]{"A", "E", "I", "O", "U"};
+        list = new ArrayList<>();
+        compare(word, "", 0);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
+        }
+
+
+        return answer;
+    }
+
+    void compare(String word, String str, int count) {
+        list.add(str);
+
+        if (count == 5) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            compare(word, str + arr[i], count + 1);
+        }
+    }
+}
