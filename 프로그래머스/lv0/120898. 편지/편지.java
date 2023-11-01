8ca4e5c6896e06bbce803257f43bac582afe7dda
Daewoon <111415442+Daewoon2@users.noreply.github.com>
2023-11-01 19:36:34 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int y = 0;
+        for (int i = 0; i <= message.length(); i++){
+            y = i;
+        }
+
+        answer = y * 2;
+
+
+        return answer;
+    }
+}
